2c2d28aaa38fa851c50d325e643f9c2ac844975b
chanadu <chandu.peddada@gmail.com>
2025-09-15 22:21:00 -0500

    remove unecessary

--- extension/src/popup.ts
+++ extension/src/popup.ts
@@ -7,12 +7,3 @@ document.getElementById('run')!.addEventListener('click', async () => {
 		files: ['content.js'],
 	});
 });
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message.action === 'updatePopup') {
-		const output = document.getElementById('output');
-		if (output) {
-			output.innerText = message.data;
-		}
-	}
-});
